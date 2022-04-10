48038b1d0e02de4817f4ffacea0739f1290e66c9
Szymon Szaminko <sz.szaminko@gmail.com>
2022-04-10 19:32:22 +0200

    Create a function to fetch data from API as an object and create HTML elements

---
+++ live-user-filter/script.js
@@ -0,0 +1,28 @@
+const result = document.querySelector("#result");
+const filter = document.querySelector("#filter");
+const listItems = [];
+
+async function getDataFromAPI() {
+  const res = await fetch("https://randomuser.me/api?results=50");
+
+  const { results } = await res.json();
+  console.log(results);
+
+  //Clear results
+  results.innerHTML = "";
+  results.forEach(user => {
+    const li = document.createElement("li");
+    listItems.push(li);
+    li.innerHTML = `
+    <img src="${user.picture.large}" alt="${user.name.first}">
+    <div class="user-info">
+        <h4>${user.name.first}</h4>
+        <p>${user.location.city}, ${user.location.country}</p>
+    </div>
+      `;
+
+    result.appendChild(li); //
+  });
+}
+
+getDataFromAPI();
