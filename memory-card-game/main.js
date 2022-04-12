2cf661f2a27e579a9c769c78e75c26fa9f650bfe
Szymon Szaminko <sz.szaminko@gmail.com>
2022-04-12 19:12:09 +0200

    Update restart function

--- memory-card-game/main.js
+++ memory-card-game/main.js
@@ -96,23 +96,32 @@ const checkCards = e => {
       playerLives--;
       playerLivesCount.textContent = playerLives;
       if (playerLives === 0) {
-        restart();
+        restart("You lose... try again!");
       }
     }
   }
+  //Run a check to see if we won the game
 };
 
 //Restart
-const restart = () => {
+const restart = text => {
   let cardData = randomize();
   let faces = document.querySelectorAll(".face");
   let cards = document.querySelectorAll(".card");
-
+  section.style.pointerEvents = "none";
   cardData.forEach((item, index) => {
     cards[index].classList.remove("toggleCard");
+    //Randomize
+    setTimeout(() => {
+      cards[index].style.pointerEvents = "all";
+      faces[index].src = item.imgSrc;
+      cards[index].setAttribute("name", item.name);
+      section.style.pointerEvents = "all";
+    }, 1000);
   });
   playerLives = 7;
   playerLivesCount.textContent = playerLives;
+  setTimeout(() => window.alert(text), 100);
 };
 
 cardGenerator();
